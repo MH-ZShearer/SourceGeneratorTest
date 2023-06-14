fe7fc929f6a5d70c20ce5cdc2ddd58a99c799505
MH-ZShearer <ZShearer@mhforce.com>
2023-06-14 14:16:49 -0400

    Add project files.

---
+++ SourceGeneratorTest/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics;
+
+namespace SourceGeneratorTest;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(TestCommand))]
+    private int _TestInt;
+
+    [CommunityToolkit.Mvvm.Input.RelayCommand]
+    private void Test()
+    {
+        Debug.WriteLine("Hello World!");
+    }
+}
